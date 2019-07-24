73271cb7a91dea84e5c7fb6db1ce9020d6d23932
zhbl <844574410@qq.com>
2019-07-24 14:26:07 +0800

    0724

--- Spider.py
+++ Spider.py
@@ -20,9 +20,49 @@ class spider(threading.Thread):
             num = self.queue.get()
             self.exec_spider(num)
 
-    def exec_spider(num):
+    def exec_spider(self,num):
         r = requests.post(url=url, headers=headers, data={'':''})
         data = json.loads(r.text)
+
+    # 判断空链接与不符合条件的结果
+    def links_check(self,link):
+        if link != '#' and link != '' and '(' not in link:
+            self.link_parse(link)
+
+    # 对获得的链接进行补全
+    def link_parse(self,r_link):
+        if '://' in r_link:
+            links.add(r_link)
+        if '://' not in r_link:
+            r_link = 'http://10.1.1.155' + r_link
+            links.add(r_link)
+        return links
+
+    def url_parse(self,url):
+        if 'http' in url:
+            try:
+                self.get_href(url)
+            except:
+                print u'所输入的地址有误或无法访问，请检查----'
+                print u'请输入这种格式：http://xxx.xxx.xx 或者 https://xxx.xxx.xx'
+            else:
+                url = 'http://' + url
+            try:
+                self.get_href(url)
+            except:
+                print u'所输入的地址有误或无法访问，请检查___'
+                print u'请输入这种格式：http://xxx.xxx.xx 或者 https://xxx.xxx.xx'
+
+    def get_href(self,url):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, 'lxml')
+        for a in soup.find_all('a'):
+            self.links_check(a['href'])
+
+    def link_check_fina(self):
+        for link in links:
+            if link != 'http://' and link != 'http://#':
+                print link
 
 
 def main(self):
@@ -41,41 +81,4 @@ if __name__ == '__main__':
 
 
 
-#判断空链接与不符合条件的结果
-def links_check(link):
-    if link != '#' and link!= '' and '(' not in link:
-        link_parse(link)
-#对获得的链接进行补全
-def link_parse(r_link):
-    if '://' in r_link:
-        links.add(r_link)
-    if '://' not in r_link:
-        r_link ='http://10.1.1.155' + r_link
-        links.add(r_link)
-    return links
 
-def url_parse(url):
-    if 'http' in url:
-        try:
-            get_href(url)
-        except:
-            print u'所输入的地址有误或无法访问，请检查----'
-            print u'请输入这种格式：http://xxx.xxx.xx 或者 https://xxx.xxx.xx'
-        else:
-            url = 'http://' +url
-        try:
-            get_href(url)
-        except:
-            print u'所输入的地址有误或无法访问，请检查___'
-            print u'请输入这种格式：http://xxx.xxx.xx 或者 https://xxx.xxx.xx'
-
-def get_href(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content,'lxml')
-    for a in soup.find_all('a'):
-        links_check(a['href'])
-
-def link_check_fina():
-    for link in links:
-        if link !='http://' and link != 'http://#':
-            print link
